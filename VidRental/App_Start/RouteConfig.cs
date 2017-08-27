5c1ebfc6bda19b5aad28e20c6f7fa963ca22e007
JohnMorris4 <johnmorris@morrisje.com>
2017-08-27 07:27:17 -0400

    Moved navbar to _Navbar and added some controller and views

--- VidRental/App_Start/RouteConfig.cs
+++ VidRental/App_Start/RouteConfig.cs
@@ -12,7 +12,15 @@ namespace VidRental
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //When creating Routes make sure that they taka a top down approach from most specific to most generic
 
+            routes.MapMvcAttributeRoutes();
+            /*routes.MapRoute(
+                "MoviesByReleseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = Old design "ByReleaseDate"},
+                new { year = @"\d{4}", month = @"\d{2}"});
+    */
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
